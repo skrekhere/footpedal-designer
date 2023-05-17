c91c86900c02abc3275672c631bc7a5e349da8e3
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2023-05-17 00:48:35 -0700

    cleaned up the UI

---
+++ godot/Nodes/mixer_node.gd
@@ -0,0 +1,21 @@
+extends GraphNode
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_slot(0, true, Globals.CONNECTOR_AUDIOSTREAM, Globals.CONNECTOR_AUDIOSTREAM_COLOUR, false, Globals.CONNECTOR_DISABLED, Globals.CONNECTOR_DISABLED_COLOUR)
+
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_close_request():
+	queue_free() # Replace with function body.
+
+
+func _on_resize_request(new_minsize):
+	custom_minimum_size = new_minsize# Replace with function body.
